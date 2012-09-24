b4ebfd2322f13359e418b049e4da8fd0c9e8aa72
Billie Zhang <billiecoder@gmail.com>
2012-09-24 22:56:49 +0800

    now capistrano works

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ server "192.168.56.10", :web, :app, :db, :primary => true
 set :application, "onestep"
 set :user, "deployer"
 set :deploy_to, "/home/#{user}/#{application}"
-#set :deploy_via, :remote_cache
+set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
@@ -14,17 +14,17 @@ set :branch, "master"
 
 default_run_options[:pty] = true
 
-#after "deploy", "deploy:cleanup"
+after "deploy", "deploy:cleanup"
 
 namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
-    task command, roles: :app, except: {no_release: true} do
+    task command, :roles => :app, except: {no_release: true} do
       run "/etc/init.d/unicorn_#{application} #{command}"
     end
   end
 
-  task :setup_config, roles: :app do
+  task :setup_config, :roles => :app do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
@@ -33,13 +33,13 @@ namespace :deploy do
   end
   after "deploy:setup", "deploy:setup_config"
 
-  task :symlink_config, roles: :app do
+  task :symlink_config, :roles => :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
   desc "Make sure local git is in sync with remote."
-  task :check_revision, roles: :web do
+  task :check_revision, :roles => :web do
     unless `git rev-parse HEAD` == `git rev-parse origin/master`
       puts "Warning: HEAD is not the same as origin/master"
       puts "Run `git push` to sync changes."
--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -13,7 +13,7 @@ set -e
 # Feel free to change any of the following variables for your app:
 AS_USER=deployer
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/$AS_USER/onestep/
+APP_ROOT=/home/$AS_USER/onestep/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 set -u
