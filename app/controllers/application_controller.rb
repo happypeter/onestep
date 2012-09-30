f69d5e21f2557902e3b131f8589ca768202f566c
Peter Wang <happypeter1983@gmail.com>
2012-09-30 16:31:44 +0800

    i18n is easy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || "zh-CN" # default to chinese, need http://casts.sme.com/?locale=en to switch to English
+  end
+
   private
 
   def current_user
