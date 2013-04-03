e5d7c7c8bba90489a4c174a358721d7eb8987e27
Peter Wang <happypeter1983@gmail.com>
2013-04-03 21:23:43 +0800

    same avatar in the same upyun bucket

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -12,8 +12,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   storage :upyun
   self.upyun_username = Settings.upyun.operator_name
   self.upyun_password = Settings.upyun.operator_password
-  self.upyun_bucket = Settings.upyun.avatar_bucket
-  self.upyun_bucket_domain = Settings.upyun.avatar_bucket_domain
+  self.upyun_bucket = Settings.upyun.bucket
+  self.upyun_bucket_domain = Settings.upyun.bucket_domain
 
   process :resize_to_fit => [420, 420]
 
