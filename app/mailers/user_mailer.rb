54ef5b4e51d53236dd578e98c066455cc35e77a9
Billie Zhang <billiecoder@gmail.com>
2013-07-07 11:43:41 +0800

    password_reset

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,13 @@
+class UserMailer < ActionMailer::Base
+  default from: "no-reply@haoqicat.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.password_reset.subject
+  #
+  def password_reset(user)
+    @user = user
+    mail :to => user.email, :subject => "Password reset"
+  end
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,8 @@ Onestep::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.default_url_options = { :host => "haoqicat.com" }
 
   # Enable threaded mode
   # config.threadsafe!
