22814d8a835a2bebef634ad4d67b44b657229053
Billie Zhang <billiecoder@gmail.com>
2013-08-10 14:32:20 +0800

    show activities related to single course

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,5 @@
 class ActivitiesController < ApplicationController
   def index
-    @activities = Activity.order("created_at desc")
+    @activities = Activity.where(course_id: params[:course_id]).order("created_at desc")
   end
 end
