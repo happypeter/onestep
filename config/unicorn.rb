7bafb25ef53f05643403e648cc8191686bc2458b
Peter Wang <happypeter1983@gmail.com>
2013-08-23 02:34:12 +0000

    we're not useing nginx&unicorn, but passenger&apache

--- config/unicorn.rb
+++
@@ -1,9 +0,0 @@
-root = "/home/deployer/onestep/current"
-working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
-
-listen "/tmp/unicorn.onestep.sock"
-worker_processes 2
-timeout 30
