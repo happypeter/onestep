a8f880288761701f19f627303c7b388ccf699aa6
Billie Zhang <billiecoder@gmail.com>
2013-09-04 17:14:35 +0800

    watch course button shows after user signed in

---
+++ app/assets/javascripts/courses.js
@@ -0,0 +1,26 @@
+(function(){
+  window.Courses = {
+    watch: function(el) {
+      var watched, course_user, course_name;
+      course_user = $(el).data("user");
+      course_name = $(el).data("course");
+      watched = $(el).data("watched");
+      if (watched) {
+        $.ajax({
+          url: "/" + course_user + "/" + course_name + "/unwatch",
+          type: "POST"
+        });
+        $(el).data("watched", false);
+        $(el).html("关注课程");
+      } else {
+        $.ajax({
+          url: "/" + course_user + "/" + course_name + "/watch",
+          type: "POST"
+        });
+        $(el).data("watched", true);
+        $(el).html("取消关注课程");
+      }
+      return false;
+    }
+  }
+}());
