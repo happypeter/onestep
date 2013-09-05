c5fa771e67c397600c39be502984446d261cd19c
Billie Zhang <billiecoder@gmail.com>
2013-09-05 16:00:58 +0800

    course watchers change with the state of course watch button

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -2,6 +2,7 @@
   window.Courses = {
     watch: function(el) {
       var watched, course_user, course_name;
+      var watchers = $(".course-watchers").text();
       course_user = $(el).data("user");
       course_name = $(el).data("course");
       watched = $(el).data("watched");
@@ -12,6 +13,7 @@
         });
         $(el).data("watched", false);
         $(el).html("关注课程");
+        $(".course-watchers").html(+watchers - 1);
       } else {
         $.ajax({
           url: "/" + course_user + "/" + course_name + "/watch",
@@ -19,6 +21,7 @@
         });
         $(el).data("watched", true);
         $(el).html("取消关注");
+        $(".course-watchers").html(+watchers + 1);
       }
       return false;
     }
