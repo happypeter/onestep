e36ea38841e3ccf5345afb4b533445db771f456c
Liu Jiyang <2411mail@gmail.com>
2013-09-10 21:09:48 +0800

    refactor course show page STEP1: add page header #348

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -2,7 +2,7 @@
   window.Courses = {
     watch: function(el) {
       var watched, course_user, course_name;
-      var watchers = $(".course-watchers").text();
+      var watchers = $(".social-count").text();
       course_user = $(el).data("user");
       course_name = $(el).data("course");
       watched = $(el).data("watched");
@@ -13,7 +13,7 @@
         });
         $(el).data("watched", false);
         $(el).html("关注课程");
-        $(".course-watchers").html(+watchers - 1);
+        $(".social-count").html(+watchers - 1);
       } else {
         $.ajax({
           url: "/" + course_user + "/" + course_name + "/watch",
@@ -21,7 +21,7 @@
         });
         $(el).data("watched", true);
         $(el).html("取消关注");
-        $(".course-watchers").html(+watchers + 1);
+        $(".social-count").html(+watchers + 1);
       }
       return false;
     }
