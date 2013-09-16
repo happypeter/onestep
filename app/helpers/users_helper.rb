737aa4fdc46266ae27457a0293d81125566b8bf9
Billie Zhang <billiecoder@gmail.com>
2013-09-16 19:05:25 +0800

    show user followers and following

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,14 +3,14 @@ module UsersHelper
     class_name = "follow"
     text = "follow"
     if current_user.blank?
-      return link_to text, login_url, :class => "minibutton"
+      return link_to text, login_url, :class => "button"
     end
     if current_user.following?(user)
       class_name = "followed"
       text = "unfollow"
     end
     link_to text, "#", :onclick => "return Users.follow(this);",
-            :class => "minibutton",
+            :class => "button",
             'data-user' => user.name,
             'data-followed' => (class_name == "followed")
   end
