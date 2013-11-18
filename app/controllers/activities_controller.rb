74bed3b1f2503b3ee0935eae84fb7c24844ce9e7
Billie Zhang <billiecoder@gmail.com>
2013-11-18 22:52:21 +0800

    course timeline style

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,8 +1,21 @@
 class ActivitiesController < ApplicationController
   def index
     user = User.find_by_name(params[:member_name])
-    course = Course.where(:user_id => user.id, :name => params[:course_name]).first
-    @activities = Activity.where(course_id: course.id).order("created_at desc")
+    course = Course.where(user_id: user.id, name: params[:course_name]).first
+    @groups = []
+    @date = []
+    activities = Activity.where(course_id: course.id).reverse
+    first = activities.first.created_at
+    date = Time.zone.now
+    while first < date do
+      group = Activity.where(course_id: course.id).within_range(date).reverse
+      if group.present?
+        @groups << group
+        @date << date
+      end
+      date -= 30.days
+    end
+    @date << date
   end
 
   def timeline
