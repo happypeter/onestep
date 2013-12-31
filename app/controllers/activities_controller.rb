78f82592884237e2b7acc63488a306e5025e4f25
Billie Zhang <billiecoder@gmail.com>
2013-12-31 17:29:24 +0800

    user timeline

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -22,7 +22,7 @@ class ActivitiesController < ApplicationController
   def timeline
     authors = []
     @activity_count = 0
-    Activity.all.collect(&:course_id).uniq.sort.each do |i|
+    Activity.all.collect(&:course_id).uniq.each do |i|
       c = Course.find(i)
       if c.public
         authors << c.user.name
@@ -43,5 +43,28 @@ class ActivitiesController < ApplicationController
       date -= 30.days
     end
     @date << date
+  end
+
+  def member_story
+    @user = User.find_by_name(params[:member_name])
+    @groups = []
+    @date = []
+    @activity_count = 0
+    if Activity.where(user_id: @user.id).present?
+      Activity.where(user_id: @user.id).collect(&:course_id).uniq.each do |i|
+        @activity_count += Activity.where(course_id: i).count if Course.find(i).public
+      end
+      first = Activity.where(user_id: @user.id).first.created_at
+      date = Time.zone.now
+      while first < date do
+        group = Activity.where(user_id: @user.id).within_range(date).reverse.cluster{ |activity| activity.course_id }
+        if group.present?
+          @groups << group
+          @date << date
+        end
+        date -= 30.days
+      end
+      @date << date
+    end
   end
 end
