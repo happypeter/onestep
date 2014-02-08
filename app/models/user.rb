1c160fcd9691e1d011e1257a8aef511dbf66cc64
Billie Zhang <billiecoder@gmail.com>
2014-02-08 17:55:07 +0800

    optimize code on user avatar uploding fix #486

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,20 +33,6 @@ class User < ActiveRecord::Base
 
   after_update :reprocess_avatar, :if => :cropping?
 
-  # define class that extends IO with methods that are required by carrierwave
-  class CarrierStringIO < StringIO
-    def original_filename
-      # the real name does not matter
-      "photo.jpeg"
-    end
-
-    def content_type
-      # real content type, "image/png" or "image/jpeg"
-      # data.match(/data:(.*);/)[1]
-      "image/jpeg"
-    end
-  end
-
   # this method will be called during standard assignment in your controller
   # (like `update_attributes`)
   def image_data=(data)
---
+++ config/initializers/stringiohax.rb
@@ -0,0 +1,13 @@
+# define class that extends IO with methods that are required by carrierwave
+  class CarrierStringIO < StringIO
+    def original_filename
+      # the real name does not matter
+      "photo.jpeg"
+    end
+
+    def content_type
+      # real content type, "image/png" or "image/jpeg"
+      # data.match(/data:(.*);/)[1]
+      "image/jpeg"
+    end
+  end
