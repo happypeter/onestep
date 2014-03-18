98fcb4cb1cf1ff56501d4fcdd23dac64b14e5c0f
Liu Jiyang <2411mail@gmail.com>
2014-03-18 21:16:58 +0800

    follow i18n on personal page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  def current_translations
+    I18n.backend.send(:init_translations) unless I18n.backend.initialized?
+    @translations ||= I18n.backend.send(:translations)
+    @translations[I18n.locale].with_indifferent_access
+  end
+
   def markdown(text)
     renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true)
     options = {
