694afa7ba4e93596c12d03e75c66572fbbf2f782
Liu Jiyang <2411mail@gmail.com>
2014-04-05 16:13:27 +0800

    rm useless codes

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,6 @@ class User < ActiveRecord::Base
 
   mount_uploader :avatar, AvatarUploader
 
-  attr_accessible :crop_x, :crop_y, :crop_w, :crop_h
   attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
 
   validates_presence_of :name, :email
