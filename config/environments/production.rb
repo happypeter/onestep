bb652c13bb3e03bf4c35c92a1ca7c4a0571b06d0
han.bing <algo31031@hotmail.com>
2014-05-30 11:55:27 +0800

    refactoring config about assets precompil; #536 fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Onestep::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( comments.js, i_fileupload.js )
+  config.assets.precompile += %w( i_fileupload.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
