b63098f84025394132d42cfdaeeb14a2f48b347e
Billie Zhang <billiecoder@gmail.com>
2014-11-18 15:58:59 +0800

    fix #564

--- config/routes.rb
+++ config/routes.rb
@@ -81,7 +81,6 @@ Onestep::Application.routes.draw do
   get "/:member_name/:course_name(/:position)" => "courses#show", :constraints => {:position => /\d+/} # "/:xxx/:xxx" will conflict with many things, so have to put bottom
   get "/:member_name/:course_name/:position/edit" => "courses#edit_video"
   get "/:member_name/:course_name/add_video" => "courses#add_video"
-  patch "/update_video/:id" => "videos#update"
+  post "/update_video/:id" => "videos#update"
   post "/sort_videos" => "videos#sort"
-
 end
