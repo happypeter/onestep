e6d542f401de1004c87c498b7d98efa5594cddfa
Peter Wang <happypeter1983@gmail.com>
2015-01-14 03:49:27 +0000

    add nginx.conf

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 20140521052220) do
     t.integer  "user_id"
     t.integer  "trackable_id"
     t.string   "trackable_type"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "course_id"
   end
 
@@ -32,15 +32,15 @@ ActiveRecord::Schema.define(version: 20140521052220) do
     t.integer  "size"
     t.string   "content_type"
     t.string   "filename"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "collaboratings", force: true do |t|
     t.integer  "course_id"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "collaboratings", ["course_id"], name: "index_collaboratings_on_course_id", using: :btree
@@ -48,8 +48,8 @@ ActiveRecord::Schema.define(version: 20140521052220) do
 
   create_table "comments", force: true do |t|
     t.text     "content"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "commentable_id"
     t.string   "commentable_type"
@@ -60,8 +60,8 @@ ActiveRecord::Schema.define(version: 20140521052220) do
   create_table "courses", force: true do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "title"
     t.string   "poster"
     t.integer  "user_id"
@@ -72,8 +72,8 @@ ActiveRecord::Schema.define(version: 20140521052220) do
   create_table "notifications", force: true do |t|
     t.integer  "user_id"
     t.boolean  "unread",          default: true
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "action"
     t.integer  "notifiable_id"
     t.string   "notifiable_type"
@@ -85,8 +85,8 @@ ActiveRecord::Schema.define(version: 20140521052220) do
   add_index "notifications", ["user_id"], name: "index_notifications_on_user_id", using: :btree
 
   create_table "orders", force: true do |t|
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "notify_id"
     t.float    "total_fee"
     t.string   "trade_status"
@@ -100,8 +100,8 @@ ActiveRecord::Schema.define(version: 20140521052220) do
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "user_id"
     t.string   "name"
   end
@@ -109,8 +109,8 @@ ActiveRecord::Schema.define(version: 20140521052220) do
   create_table "relationships", force: true do |t|
     t.integer  "follower_id"
     t.integer  "followed_user_id"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "relationships", ["followed_user_id"], name: "index_relationships_on_followed_user_id", using: :btree
@@ -120,8 +120,8 @@ ActiveRecord::Schema.define(version: 20140521052220) do
   create_table "users", force: true do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "token"
     t.boolean  "admin"
     t.string   "name"
@@ -133,8 +133,8 @@ ActiveRecord::Schema.define(version: 20140521052220) do
 
   create_table "videos", force: true do |t|
     t.string   "title"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "course_id"
     t.integer  "position"
     t.text     "desc"
@@ -150,8 +150,8 @@ ActiveRecord::Schema.define(version: 20140521052220) do
   create_table "watchings", force: true do |t|
     t.integer  "user_id"
     t.integer  "course_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "watchings", ["course_id"], name: "index_watchings_on_course_id", using: :btree
