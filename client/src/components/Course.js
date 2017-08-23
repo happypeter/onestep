84132c99737f7cb5deca72b467ba7088ca3fd1d9
BeijiYang <langyuyang@outlook.com>
2017-08-24 00:55:34 +0800

    add chapter list

--- client/src/components/Course.js
+++ client/src/components/Course.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import '../style/course.css'
 
 class Course extends Component {
   render(){
+    let { id } = this.props.match.params
+    let thisCourse = this.props.courses.find(
+      item => (
+        item.id === id
+      )
+    )
+    let chapter = thisCourse.chapter.map((item, i) => (
+      <Link to={`/course/${id}/${item}`} key={i} className='chapter'>
+        {item}
+      </Link>
+    ))
+
     return(
-      <div className="home">
-        Course
+      <div className="chapter-list">
+        <img src={`${thisCourse.post}`} alt='poster' className='poster' />
+        <h2>章节列表</h2>
+        { chapter }
       </div>
     )
   }
 }
 
-export default Course
+const mapStateToProps = (state) => ({
+  courses: state
+})
+
+export default connect(mapStateToProps)(Course)
--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -4,19 +4,37 @@ let courses = [
     'id': '0',
     'title': 'Course0',
     'post': 'http://haoqicat-1253322599.costj.myqcloud.com/posters/redux-hello-v2.png',
-    'msg': 'peter - 2days ago'
+    'msg': 'peter - 2days ago',
+    'chapter': [
+      'course0 - chapter0 - 00000',
+      'course0 - chapter1 - 00000',
+      'course0 - chapter2 - 00000',
+      'course0 - chapter3 - 00000'
+    ]
   },
   {
     'id': '1',
     'title': 'Course1',
     'post': 'http://haoqicat-1253322599.costj.myqcloud.com/posters/aa-journey.png',
-    'msg': 'peter - 12days ago'
+    'msg': 'peter - 12days ago',
+    'chapter': [
+      'course1 - chapter0 - 111111',
+      'course1 - chapter1 - 111111',
+      'course1 - chapter2 - 111111',
+      'course1 - chapter3 - 111111'
+    ]
   },
   {
     'id': '2',
     'title': 'Course2',
     'post': 'http://haoqicat-1253322599.costj.myqcloud.com/posters/redux-hello-v2.png',
-    'msg': 'peter - 22days ago'
+    'msg': 'peter - 22days ago',
+    'chapter': [
+      'course2 - chapter0 - 233333',
+      'course2 - chapter1 - 233333',
+      'course2 - chapter2 - 233333',
+      'course2 - chapter3 - 233333'
+    ]
   }
 ]
 
