664f490b4d229b79eb691c6a9c7c02a619aee3d9
BeijiYang <langyuyang@outlook.com>
2017-08-23 22:03:46 +0800

    css for button/link

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import { Route, Link } from 'react-router-dom'
 import CourseList from './CourseList'
+import '../style/home.css'
 
 export default () => (
   <div className='home'>
-    <Link to='/Login'>微信登录</Link>
+    <Link to='/Login' className='button'>微信登录</Link>
     <CourseList />
   </div>
 )
