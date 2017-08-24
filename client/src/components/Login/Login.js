efe3225e2e5e5d848d5caeb23180f8b170749715
BeijiYang <langyuyang@outlook.com>
2017-08-24 22:29:35 +0800

    standard style

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -5,32 +5,31 @@ import './login.css'
 
 class Login extends Component {
 
- login = () => {
-   this.props.dispatch({ type: 'IS_AUTH' })
-   this.props.dispatch({ type: 'TO_REFERRER' })
- }
-
- render() {
-   const { redirectToReferrer } = this.props
-   const refererState = this.props.location.state
-
-
-   if (redirectToReferrer && refererState ) {
-     let refererPath = refererState.from.pathname
-     return (
-       <Redirect to={refererPath} />
-     )
-   }else if(redirectToReferrer) {
-     return (
-       <Redirect to='/' />
-     )
+  login = () => {
+     this.props.dispatch({ type: 'IS_AUTH' })
+     this.props.dispatch({ type: 'TO_REFERRER' })
    }
-   return (
-     <div className="home">
-       <button onClick={this.login} className='fake-code'>点击假装微信扫码登录</button>
-     </div>
-   )
- }
+
+  render () {
+    const { redirectToReferrer } = this.props
+    const refererState = this.props.location.state
+
+    if (redirectToReferrer && refererState) {
+      let refererPath = refererState.from.pathname
+      return (
+        <Redirect to={refererPath} />
+      )
+    } else if (redirectToReferrer) {
+      return (
+        <Redirect to='/' />
+      )
+    }
+    return (
+      <div className='home'>
+        <button onClick={this.login} className='fake-code'>点击假装微信扫码登录</button>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = (state) => ({
