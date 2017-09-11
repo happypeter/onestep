52bce06c8f92e1010805d2e28b7ef01a479f91a4
BeijiYang <langyuyang@outlook.com>
2017-09-11 16:12:28 +0800

    install antd & rm useless code

--- api/src/App.js
+++ api/src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Button from 'antd/lib/button';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Button type="primary">Button</Button>
       </div>
     );
   }
