3cf9bbd65417d6e289c9771a55be63eee110ec83
BeijiYang <langyuyang@outlook.com>
2017-09-13 10:28:27 +0800

    test api

--- admin/src/component/test.js
+++ admin/src/component/test.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import config from '../config'
 import { Layout, Breadcrumb, Icon } from 'antd'
 const { Header, Content, Footer } = Layout
 
 class Test extends Component {
+  constructor (props, context) {
+    super(props, context)
+    this.state = {
+      value: '',
+      test: 'TEST'
+    }
+  }
+  componentWillMount(){
+    axios.get(`${config.api}`)
+    .then(
+      res => {
+        this.setState({value:res.data})
+        console.log(this.state.value);
+      }
+    )
+    .catch(err => {
+      if (err.response) {
+        console.log(err.response.data.err)
+      } else {
+        console.log(err)
+      }
+    })
+  }
   render() {
     return (
       <Layout style={{ minHeight: '100vh', width: '100%' }}>
@@ -13,11 +38,11 @@ class Test extends Component {
 
         <Content style={{ margin: '0 16px' }}>
           <Breadcrumb style={{ margin: '12px 0' }}>
-            <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            <Breadcrumb.Item>Demo</Breadcrumb.Item>
+            <Breadcrumb.Item>Test</Breadcrumb.Item>
           </Breadcrumb>
           <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-            Bill is a cat.
+            value: {this.state.value}
           </div>
         </Content>
 
