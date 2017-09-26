5f2d765dfabfa99f4c43b98ef1c84ad79b88415b
BeijiYang <langyuyang@outlook.com>
2017-09-26 10:43:35 +0800

    create react app basic

---
+++ admin/src/components/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './app.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        hello world war
+      </div>
+    );
+  }
+}
+
+export default App
