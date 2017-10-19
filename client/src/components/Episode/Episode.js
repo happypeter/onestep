d9116d9a82d31fddb00203041a848d7f98c761c5
BeijiYang <langyuyang@outlook.com>
2017-10-19 13:54:03 +0800

    refactory Episode : styled-components

--- client/src/components/Episode/Episode.js
+++ client/src/components/Episode/Episode.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
-import './episode.css'
+import styled from 'styled-components'
 
+const EpisodeWrap = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  padding-top: 20px;
+  background-color: #B2EBF2;
+  text-align: center;
+`
+const VideoWrap = styled.div`
+  border: 1px solid #BDBDBD;
+  margin: 10%;
+  padding: 150px 200px;
+  background-color: #0097A7;
+`
 class Episode extends Component {
   render () {
     return (
-      <div className='episode-video'>
-        <div className='video'>
+      <EpisodeWrap>
+        <VideoWrap>
           video: {this.props.episodeName}
-        </div>
-      </div>
+        </VideoWrap>
+      </EpisodeWrap>
     )
   }
 }
