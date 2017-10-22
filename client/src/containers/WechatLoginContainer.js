e5eb07209ec67fc3dc4cf164bce2d00213f24cb2
BeijiYang <langyuyang@outlook.com>
2017-10-22 23:05:39 +0800

    refactory: rm mapDispatchToProps

--- client/src/containers/WechatLoginContainer.js
+++ client/src/containers/WechatLoginContainer.js
@@ -2,19 +2,16 @@ import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import WechatLogin from '../components/Login/WechatLogin'
-// import { wechatLogin } from '../redux/actions/authAction'
-import { setCurrentUserInfo } from '../redux/actions/authAction'
+import { wechatLogin } from '../redux/actions/authAction'
 import PropTypes from 'prop-types'
 
 class WechatLoginContainer extends Component {
 
   login = () => {
-    //  this.props.dispatch({
-    //    type: 'AUTH_USER',
-    //    userInfo: {username: 'wechatCode', password: ''}
-    //  })
-    //   window.localStorage.setItem('userInfo', 'wechatCode')
-    this.props.wechatLogin({username: 'wechatCode', password: ''})
+    this.props.wechatLogin({
+      username: 'wechatCode',
+      password: ''
+    })
    }
 
   render () {
@@ -41,25 +38,8 @@ WechatLoginContainer.propTypes = {
   wechatLogin: PropTypes.func.isRequired
 }
 
-const mapDispatchToProps = function(dispatch, ownProps) {
-  return {
-
-    wechatLogin:  (data) => {
-
-        // axios ...
-        // dispatch({
-        //   type: 'AUTH_USER',
-        //   userInfo: {username: 'wechatCode', password: ''}
-        // })
-        dispatch(setCurrentUserInfo(data))
-        window.localStorage.setItem('userInfo', 'wechatCode')
-
-    }
-}
-}
-
 const mapStateToProps = (state) => ({
   isAuthenticated: state.fakeAuth.isAuthenticated
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(WechatLoginContainer)
+export default connect(mapStateToProps, { wechatLogin })(WechatLoginContainer)
