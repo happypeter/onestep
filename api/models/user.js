ef1c564c4cad7a0662dbf064fdd77e3dd192770c
BeijiYang <langyuyang@outlook.com>
2017-11-06 10:53:43 +0800

    fix a bug: difference between bcryptjs and bcrypt-nodejs

--- api/models/user.js
+++ api/models/user.js
@@ -22,7 +22,7 @@ UserSchema.pre('save', function (next) {
   const SALT_FACTOR = 5
   bcrypt.genSalt(SALT_FACTOR, function (err, salt) {
     if (err) return next(err)
-    bcrypt.hash(user.password, salt, null, function (err, hash) {
+    bcrypt.hash(user.password, salt, function (err, hash) {
       if (err) return next(err)
       user.password = hash
       next()
