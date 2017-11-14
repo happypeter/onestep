ffeafef828b4de600e02e2a66ab828628b21fd15
BeijiYang <langyuyang@outlook.com>
2017-11-14 18:09:37 +0800

    add react-tap-event-plugin

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './containers/App'
-// import injectTapEventPlugin from 'react-tap-event-plugin'
-//
-// injectTapEventPlugin()
+import injectTapEventPlugin from 'react-tap-event-plugin'
+
+injectTapEventPlugin()
 
 ReactDOM.render(<App />, document.getElementById('root'))
