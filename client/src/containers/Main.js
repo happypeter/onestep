904e2e9ee778e2b6851c6a484e47035ce70836a3
BeijiYang <langyuyang@outlook.com>
2017-11-20 10:43:14 +0800

    code splitting in react router

--- client/src/containers/Main.js
+++ client/src/containers/Main.js
@@ -1,31 +1,33 @@
 import React, { Component } from 'react'
-import Home from '../components/Home/Home'
-import WechatLogin from './WechatLoginContainer'
-import Login from './LoginContainer'
-import Course from './CourseContainer'
-import Episode from './EpisodeContainer'
-import Signup from './SignupContainer'
-import Profile from './ProfileContainer'
-import {requireAuthentication} from './CheckToken'
-
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from 'react-router-dom'
 
+import {requireAuthentication} from './CheckToken'
+import asyncComponent from './AsyncComponent'
+
+const AsyncHome = asyncComponent(() => import('../components/Home/Home'))
+const AsyncLogin = asyncComponent(() => import('./LoginContainer'))
+const AsyncCourse = asyncComponent(() => import('./CourseContainer'))
+const AsyncSignup = asyncComponent(() => import('./SignupContainer'))
+const AsyncEpisode = asyncComponent(() => import('./EpisodeContainer'))
+const AsyncProfile = asyncComponent(() => import('./ProfileContainer'))
+const AsyncWechatLogin = asyncComponent(() => import('./WechatLoginContainer'))
+
 class Main extends Component {
   render () {
     return (
       <Router>
         <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/wechatLogin' component={WechatLogin} />
-          <Route path='/login' component={Login} />
-          <Route path='/signup' component={Signup} />
-          <Route path='/profile' component={requireAuthentication(Profile)} />
-          <Route path='/:courseName/:episodeName' component={requireAuthentication(Episode)} />
-          <Route path='/:courseName' component={Course} />
+          <Route exact path='/' component={AsyncHome} />
+          <Route path='/wechatLogin' component={AsyncWechatLogin} />
+          <Route path='/login' component={AsyncLogin} />
+          <Route path='/signup' component={AsyncSignup} />
+          <Route path='/profile' component={requireAuthentication(AsyncProfile)} />
+          <Route path='/:courseName/:episodeName' component={requireAuthentication(AsyncEpisode)} />
+          <Route path='/:courseName' component={AsyncCourse} />
         </Switch>
       </Router>
     )
