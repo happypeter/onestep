4ae6e0c474842a91d6b9b205f3057a5863603348
BeijiYang <langyuyang@outlook.com>
2017-11-23 22:30:08 +0800

    refactor style of TopHeader

--- client/src/components/Header/TopHeader.js
+++ client/src/components/Header/TopHeader.js
@@ -3,28 +3,42 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 const TopHeaderWrap = styled.div`
-  background-color: #00BCD4;
+  background-color: #FFFFFF;
   display: flex;
   justify-content: space-between;
+
 `
 
 const SideButtonsWrap = styled.div`
   display: flex;
   flex-direction: flex-end;
+  padding: 1em;
+  @media (min-width: 1024px) {
+    padding: 1em 6em;
+  }
 `
 
 const Button = styled(Link)`
   font-size: 1em;
   padding: 0.5em;
-  color: white;
+  color: #212121;
   line-height: 2;
   opacity: 0.8;
   transition: all 0.5s ease;
-  font-weight: 600;
   text-decoration: none;
-  @media (min-width: 850px) {
-    font-size: 1.2em;
-    padding: 0.5em 1.3em;
+  @media (min-width: 1024px) {
+    font-size: 1em;
+    padding: 0.5em;
+  }
+`
+
+const HomeWrap = styled.div`
+  font-size: 1.56em;
+  display: flex;
+  align-items: center;
+  margin-left: 1em;
+  @media (min-width: 1024px) {
+    margin-left: 4em;
   }
 `
 
@@ -62,9 +76,12 @@ class TopHeader extends Component {
 
     return (
       <TopHeaderWrap>
-        <Button to='/'>
-          首页
-        </Button>
+        <HomeWrap>
+          <img src={require('../../assets/logoSimple.svg')} alt='logo-simple' width='55px' />
+          <Button to='/'>
+            首页
+          </Button>
+        </HomeWrap>
         {
           this.props.sideButtons
           ? LogoutButtons
