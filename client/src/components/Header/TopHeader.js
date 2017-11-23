7275e6f022bae992a992324fdf08cef148280ab2
BeijiYang <langyuyang@outlook.com>
2017-11-23 22:38:03 +0800

    TopHeader

--- client/src/components/Header/TopHeader.js
+++ client/src/components/Header/TopHeader.js
@@ -23,8 +23,21 @@ const Button = styled(Link)`
   padding: 0.5em;
   color: #212121;
   line-height: 2;
-  opacity: 0.8;
   transition: all 0.5s ease;
+  text-decoration: none;
+  @media (min-width: 1024px) {
+    font-size: 1em;
+    padding: 0.5em;
+  }
+`
+
+const Username = styled(Link)`
+  font-size: 1em;
+  padding: 0.5em;
+  color: rgb(255, 226, 0);
+  line-height: 2;
+  transition: all 0.5s ease;
+  font-weight: 600;
   text-decoration: none;
   @media (min-width: 1024px) {
     font-size: 1em;
@@ -39,21 +52,6 @@ const HomeWrap = styled.div`
   margin-left: 1em;
   @media (min-width: 1024px) {
     margin-left: 4em;
-  }
-`
-
-const Username = styled(Link)`
-  font-size: 1em;
-  padding: 0.5em;
-  color: rgb(255, 226, 0);
-  line-height: 2;
-  opacity: 0.8;
-  transition: all 0.5s ease;
-  font-weight: 600;
-  text-decoration: none;
-  @media (min-width: 850px) {
-    font-size: 1.2em;
-    padding: 0.5em 1.3em;
   }
 `
 
