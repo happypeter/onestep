d8d17794470e446150460b502a2dd2cda365581d
BeijiYang <langyuyang@outlook.com>
2017-12-04 10:07:26 +0800

    Course page

--- client/src/components/Course/Course.js
+++ client/src/components/Course/Course.js
@@ -2,53 +2,290 @@ import React, { Component } from 'react'
 import TopHeader from '../../containers/TopHeaderContainer'
 import Footer from '../Footer/Footer'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+// import RaisedButton from 'material-ui/RaisedButton'
+import Button from 'material-ui/Button';
+import ChapterIcon from '../../assets/ChapterIcon.svg'
+import EpisodeIcon from '../../assets/EpisodeIcon.svg'
+import VideoPlayer from '../../lib/videoPlayer/VideoPlayer'
 
-const CourseWrap = styled.div`
+const Wrap = styled.div`
   display: flex;
   flex-direction: column;
-  background-color: #B2EBF2;
-  min-height: 100vh;
-  margin: 0 auto;
-  padding: 10% 20%;
-  text-align: center;
-  .episode {
-    margin: 10px;
-    padding: 5px;
-    text-decoration: none;
-    color: #FFFFFF;
-    background: #0097A7;
-    border-radius: 40px;
-  }
-`
-const IntroImg = styled.img`
-  margin-bottom: 80px;
-  border-radius: 20px;
+  font-family: PingFangSC-Regular;
+  max-width: 1350px;
+  p {
+  ${'' /* font-size: 50px; */}
+  font-family: -apple-system, BlinkMacSystemFont,
+    "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell",
+    "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+}
+p.thin {
+  font-weight: 100;
+}
+p.normal {
+  font-weight: normal;
+}
+p.thick {
+  font-weight: bold;
+}
 `
 
-// const EpisodesWrap = styled.div`
-//   margin: 10px;
-//   padding: 5px;
-//   text-decoration: none;
-//   color: #FFFFFF;
-//   background: #0097A7;
-//   border-radius: 40px;
-// `
+const VideoWrap = styled.div`
+
+`
+
+const VideoTitle = styled.div`
+  height: 2.5em;
+  line-height: 2.5em;
+  max-width: 100%;
+  background-color: #00BCD4;
+  color: #FFFFFF;
+  font-size: 1.25em;
+  padding-left: 12px;
+`
+
+const Video = styled.div`
+  margin: 0 23%;
+`
+
+const MsgWrap = styled.div`
+  display: flex;
+  background-color: #F7F7F7;
+  flex-shrink: 0;
+`
+
+const MsgArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 23%;
+  min-width: 600px;
+`
+
+const MsgIntro = styled.div`
+  color: #212121;
+  margin: 0px auto;
+  margin-top: 44px;
+  margin-left: 0;
+  margin-right: 10px;
+  padding: 0.5em 1em;
+  margin-bottom: 14px;
+  font-size: 1em;
+  border-left: 7px solid #00BCD4;
+`
+
+const MsgBigCard = styled.div`
+  width: 400px;
+  max-height: 20.5em;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  margin-right: 20%;
+  margin-top: 44px;
+  margin-bottom: 2em;
+  padding-bottom: 1em;
+  box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.25);
+  background-color: white;
+  -webkit-transition: all 450ms ease;
+  transition: all 450ms ease;
+  p {
+    font-weight: 300;
+    font-size: 0.875em;
+    color: #212121;
+  }
+`
+
+const Splitter = styled.div`
+  flex-shrink: 0;
+  height: 1px;
+  margin: 0 16%;
+  opacity: 0.48;
+  background: #D8D8D8;
+`
+
+const RaisedButtonWrap = styled(Button)`
+  && {
+    margin: 1em 16%;
+    color: white;
+    background-color: #00B4D0;
+  }
+`
+
+const PriceArea = styled.div`
+  background-color: #00BCD4;
+  height: 40%;
+  padding: 1.75em;
+  color: #FFFFFF;
+  p {
+    font-weight: 200;
+    font-size: 1em;
+    color: #FFFFFF;
+  }
+`
+
+const Price = styled.div`
+  font-size: 3em;
+  margin: 10px 30px;
+`
+
+const MsgSmallCards = styled.div`
+  display: flex;
+`
+
+const MsgSmallCard = styled.div`
+  width: 30%;
+  text-align: center;
+  padding: 0.5em;
+  margin-right: 20%;
+  margin-bottom: 6em;
+  box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.25);
+  background-color: white;
+  -webkit-transition: all 450ms ease;
+  transition: all 450ms ease;
+`
+
+const CardTitle = styled.span`
+  font-size: 1em;
+  color: #212121;
+`
+
+const CardContent = styled.p`
+  font-size: 0.8em;
+  line-height: 1.6em;
+  opacity: 0.5;
+  margin: 0.5em;
+  text-align: left;
+`
+
+const CatalogueWrap = styled.div`
+  margin: 6.375em 23% 3em 23%;
+  border-top: 2.7em solid #00BCD4;
+  box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.25);
+  -webkit-transition: all 450ms ease;
+  transition: all 450ms ease;
+  text-decoration: none;
+`
+
+const CourseName = styled.div`
+  margin: 44px auto;
+  color: #573D00;
+  text-align: center;
+  font-size: 2.5em;
+`
+
+const Info = styled.div`
+  margin: 0px auto;
+  text-align: center;
+  font-size: 2.5em;
+`
+const EpisodesWrap = styled.div`
+  margin: 10px;
+  padding: 5px 50px;
+  text-decoration: none;
+  text-align: left;
+  color: #212121;
+  border-radius: 40px;
+`
+
+const EpisodeChapter = styled.div`
+  display: flex;
+  color: #000000;
+  margin-bottom: 10px;
+  h1 {
+    margin-left: 29px;
+    font-size: 25px;
+    font-weight: 300;
+  }
+`
+
+const EpisodeLink = styled.div`
+  display: flex;
+  margin-left: 29px;
+  margin-bottom: 18px;
+`
+
+const EpisodeTitle = styled(Link)`
+  display: block;
+  margin-left: 43px;
+  text-decoration: none;
+  color: #000000;
+  font-size: 1em;
+  font-weight: 300;
+`
 
 class Course extends Component {
   render () {
-    let {src, episode} = this.props
+    let { name, intro, writing_to_who: writingToWho, learning_goal: learningGoal, price, publishedOn, vlink, service, content } = this.props.courseOptions
+
+    let episode
+    if (content) {
+      episode = content.map((item, i) => (
+        <EpisodesWrap to={'/'} key={item.header}>
+          <EpisodeChapter>
+            <img src={ChapterIcon} alt={'ChapterIcon'} />
+            <h1>{item.header}</h1>
+          </EpisodeChapter>
+          {
+            item.section.map(
+              t => (
+                <EpisodeLink>
+                  <img src={EpisodeIcon} alt={'EpisodeIcon'} />
+                  <EpisodeTitle to={`${this.props.courseName}/${t.link}`} key={t.link}>{t.title}</EpisodeTitle>
+                </EpisodeLink>
+              )
+            )
+          }
+        </EpisodesWrap>
+    ))
+    } else {
+      episode = (<Info>加载中</Info>)
+    }
+
     return (
-      <div>
+      <Wrap>
         <TopHeader />
-        <CourseWrap>
-          <IntroImg src={src} alt='poster' />
-          <h2>章节列表</h2>
-          {/* <EpisodesWrap> */}
-            { episode }
-          {/* </EpisodesWrap> */}
-        </CourseWrap>
+
+        <VideoWrap>
+          <CourseName>{name}</CourseName>
+          <Video>
+            <VideoTitle>课程简介</VideoTitle>
+            <VideoPlayer {...this.props.videoJsOptions} />
+          </Video>
+        </VideoWrap>
+
+        <MsgWrap>
+          <MsgArea>
+            <MsgIntro>{intro}</MsgIntro>
+            <MsgSmallCards>
+              <MsgSmallCard>
+                <CardTitle>适合观众</CardTitle>
+                <CardContent>{writingToWho}</CardContent>
+              </MsgSmallCard>
+              <MsgSmallCard>
+                <CardTitle>知识点</CardTitle>
+                <CardContent>{learningGoal}</CardContent>
+              </MsgSmallCard>
+            </MsgSmallCards>
+          </MsgArea>
+          <MsgBigCard>
+            <PriceArea>
+              <Price>{price}元</Price>
+              <p>本课程</p>
+            </PriceArea>
+            <p>课程永久学习权限</p>
+            <Splitter />
+            <p>源代码</p>
+            <RaisedButtonWrap raised>购买</RaisedButtonWrap>
+          </MsgBigCard>
+        </MsgWrap>
+
+        <CatalogueWrap>
+          { episode }
+        </CatalogueWrap>
+
         <Footer />
-      </div>
+      </Wrap>
     )
   }
 }
--- client/src/containers/CourseContainer.js
+++ client/src/containers/CourseContainer.js
@@ -6,42 +6,110 @@ import {
   matchPath
 } from 'react-router-dom'
 import Course from '../components/Course/Course'
+import { fetchCourse } from '../redux/actions/contentAction'
+
+// import Loadable from 'react-loadable'
+// import LoadingComponent from '../common/Loading'
+//
+// const AsyncHome = Loadable({
+//   loader: () => import('../components/Home/Home'),
+//   loading: LoadingComponent,
+//   delay: 300
+// })
+
 
 class CourseContainer extends Component {
-  render () {
+
+  componentWillMount () {
     let { courseName } = this.props.match.params
-    let thisCourse = this.props.courses.find(
-      item => (
-        item.title === courseName
-      )
-    )
-    if (!thisCourse) {
-      return <Redirect to={{ pathname: '/404' }} />
-    }
+    // fetch the course data form server
+    this.props.fetchCourse({ courseName })
+  }
+
+  render () {
+    // console.log(this.props);
+    // 404 Redirect
+    let { courseName } = this.props.match.params
+    // console.log(courseName);
+
+    // let { catalogue } = this.props.courses
+    // console.log(catalogue);
+    // let thisCourse = this.props.courses.find(
+    // let thisCourse = catalogue.find(
+    //   item => (
+    //     item.link.substr(1) === courseName
+    //   )
+    // )
+    // if (!thisCourse) {
+    //   return <Redirect to={{ pathname: '/404' }} />
+    // }
 
     const match = matchPath(this.props.location.pathname, {
       path: this.props.match.path
     })
+    // console.log(this.props);
     if (!match.isExact) {
       return <Redirect to={{ pathname: '/404' }} />
     }
 
-    let episode = thisCourse.episode.map((item, i) => (
-      <Link to={`/${courseName}/${item}`} key={i} className='episode'>
-        {item}
-      </Link>
-    ))
+    let { status, content: courseContent } = this.props.course
 
-    return (
-      <div>
-        <Course src={thisCourse.post} episode={episode} />
-      </div>
-    )
+    switch (status) {
+      case 'LOADING': {
+        return (
+          <div>
+            <div>信息请求中...</div>
+          </div>
+        )
+      }
+      case 'SUCCESS': {
+        // VideoJsOptions for this Course
+        const CourseVideoJsOptions = {
+          autoplay: false,
+          controls: true,
+          sources: [{
+            src: `${courseContent.vlink}/index.mp4`,
+            type: 'video/mp4'
+          }],
+          poster: 'http://videojs.com/img/logo.png',
+          fluid: 'true', // put the player in the VideoPlayerWrap box
+          playbackRates: [0.75, 1, 1.5, 2],
+          controlBar: {
+            volumePanel: {
+              inline: false // vertical VolumeControl
+            }
+          },
+          // Using A Plugin
+          plugins: {
+            setStateandFocusPlugin: true
+          }
+        }
+
+        return (
+          <div>
+            <Course
+              courseOptions={courseContent}
+              videoJsOptions={CourseVideoJsOptions}
+            />
+          </div>
+        )
+      }
+      case 'FAILURE': {
+        return (
+          <div>
+            <div>信息加载失败</div>
+          </div>
+        )
+      }
+      default: {
+        throw new Error('unexpected status ' + status)
+      }
+    }
   }
 }
 
 const mapStateToProps = (state) => ({
-  courses: state.courses
+  course: state.course
 })
 
-export default connect(mapStateToProps)(CourseContainer)
+export default connect(mapStateToProps, { fetchCourse })(CourseContainer)
