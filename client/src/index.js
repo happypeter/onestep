d02eb1173db7447218a30b558485ddcd4b0794a0
BeijiYang <langyuyang@outlook.com>
2017-12-04 18:22:56 +0800

    injectGlobal

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './containers/App'
 import injectTapEventPlugin from 'react-tap-event-plugin'
+import { injectGlobal } from 'styled-components'
+
+injectGlobal`
+  html {
+    max-width: 1024px;
+    margin: 0 auto;
+  }
+  body {
+
+    font-family: PingFangSC-Regular;
+    p {
+      font-family: -apple-system, BlinkMacSystemFont,
+      "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell",
+      "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    }
+    p.thin {
+      font-weight: 100;
+    }
+    p.normal {
+      font-weight: normal;
+    }
+    p.thick {
+      font-weight: bold;
+    }
+  }
+`
 
 injectTapEventPlugin()
 
