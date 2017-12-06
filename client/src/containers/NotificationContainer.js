d20207833d2b32d735e1a2495d3b2175d9add457
BeijiYang <langyuyang@outlook.com>
2017-12-06 18:59:33 +0800

    Notification: new version

--- client/src/containers/NotificationContainer.js
+++ client/src/containers/NotificationContainer.js
@@ -2,41 +2,41 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 import Notification from '../components/Notification/Notification'
-import {
-  removeLogoutNotification,
-  removeLoginNotification,
-  removeSignupNotification,
-  removeUnhandledErrNotification,
-  removeInvalidTokenNotification,
-  removeResetPasswordNotification
-} from '../redux/actions/notificationAction'
-import PropTypes from 'prop-types'
+// import {
+//   removeLogoutNotification,
+//   removeLoginNotification,
+//   removeSignupNotification,
+//   removeUnhandledErrNotification,
+//   removeInvalidTokenNotification,
+//   removeResetPasswordNotification
+// } from '../redux/actions/notificationAction'
+// import PropTypes from 'prop-types'
 
 class NotificationContainer extends Component {
 
-  removeLogoutNotification = () => {
-    this.props.removeLogoutNotification()
-  }
-
-  removeLoginNotification = () => {
-    this.props.removeLoginNotification()
-  }
-
-  removeSignupNotification = () => {
-    this.props.removeSignupNotification()
-  }
-
-  removeUnhandledErrNotification = () => {
-    this.props.removeUnhandledErrNotification()
-  }
-
-  removeInvalidTokenNotification = () => {
-    this.props.removeInvalidTokenNotification()
-  }
-
-  removeResetPasswordNotification = () => {
-    this.props.removeResetPasswordNotification()
-  }
+  // removeLogoutNotification = () => {
+  //   this.props.removeLogoutNotification()
+  // }
+  //
+  // removeLoginNotification = () => {
+  //   this.props.removeLoginNotification()
+  // }
+  //
+  // removeSignupNotification = () => {
+  //   this.props.removeSignupNotification()
+  // }
+  //
+  // removeUnhandledErrNotification = () => {
+  //   this.props.removeUnhandledErrNotification()
+  // }
+  //
+  // removeInvalidTokenNotification = () => {
+  //   this.props.removeInvalidTokenNotification()
+  // }
+  //
+  // removeResetPasswordNotification = () => {
+  //   this.props.removeResetPasswordNotification()
+  // }
 
   render () {
     let { showLogoutNotification, showLoginNotification, showSignupNotification, showInvalidTokenNotification, showUnhandledErrNotification, showResetPasswordNotification } = this.props.notification
@@ -46,7 +46,7 @@ class NotificationContainer extends Component {
           showLogoutNotification ?
           (
             <Notification
-            removeNotification={this.removeLogoutNotification}
+            // removeNotification={this.removeLogoutNotification}
             text={'已经退出登录'}
             />
           ) :
@@ -56,7 +56,7 @@ class NotificationContainer extends Component {
           showLoginNotification ?
           (
             <Notification
-            removeNotification={this.removeLoginNotification}
+            // removeNotification={this.removeLoginNotification}
             text={'已经成功登录'}
             />
           ) :
@@ -66,7 +66,7 @@ class NotificationContainer extends Component {
           showSignupNotification ?
           (
             <Notification
-            removeNotification={this.removeSignupNotification}
+            // removeNotification={this.removeSignupNotification}
             text={'账号注册成功'}
             />
           ) :
@@ -78,7 +78,7 @@ class NotificationContainer extends Component {
             <div>
             <Redirect to='/profile' />
             <Notification
-            removeNotification={this.removeResetPasswordNotification}
+            // removeNotification={this.removeResetPasswordNotification}
             text={'重置密码成功'}
             />
            </div>
@@ -89,7 +89,7 @@ class NotificationContainer extends Component {
           (window.sessionStorage.getItem('jwtToken') && showInvalidTokenNotification) ?
           (
             <Notification
-            removeNotification={this.removeInvalidTokenNotification}
+            // removeNotification={this.removeInvalidTokenNotification}
             text={'登录过期 请重新登录'}
             />
           ) :
@@ -99,7 +99,7 @@ class NotificationContainer extends Component {
           showUnhandledErrNotification ?
           (
             <Notification
-            removeNotification={this.removeUnhandledErrNotification}
+            // removeNotification={this.removeUnhandledErrNotification}
             text={'网络错误 操作失败'}
             />
           ) :
@@ -110,24 +110,26 @@ class NotificationContainer extends Component {
   }
 }
 
-NotificationContainer.PropTypes = {
-  removeLogoutNotification: PropTypes.func.isRequired,
-  removeLoginNotification: PropTypes.func.isRequired,
-  removeSignupNotification: PropTypes.func.isRequired,
-  removeUnhandledErrNotification: PropTypes.func.isRequired,
-  removeInvalidTokenNotification: PropTypes.func.isRequired,
-  removeResetPasswordNotification: PropTypes.func.isRequired
-}
+// NotificationContainer.PropTypes = {
+//   removeLogoutNotification: PropTypes.func.isRequired,
+//   removeLoginNotification: PropTypes.func.isRequired,
+//   removeSignupNotification: PropTypes.func.isRequired,
+//   removeUnhandledErrNotification: PropTypes.func.isRequired,
+//   removeInvalidTokenNotification: PropTypes.func.isRequired,
+//   removeResetPasswordNotification: PropTypes.func.isRequired
+// }
 
 const mapStateToProps = (state) => ({
   notification: state.notification
 })
 
-export default connect(mapStateToProps,{
-  removeLogoutNotification,
-  removeLoginNotification,
-  removeSignupNotification,
-  removeUnhandledErrNotification,
-  removeInvalidTokenNotification,
-  removeResetPasswordNotification
-})(NotificationContainer)
+export default connect(mapStateToProps
+//   ,{
+//   removeLogoutNotification,
+//   removeLoginNotification,
+//   removeSignupNotification,
+//   removeUnhandledErrNotification,
+//   removeInvalidTokenNotification,
+//   removeResetPasswordNotification
+// }
+)(NotificationContainer)
--- client/src/redux/actions/notificationAction.js
+++ client/src/redux/actions/notificationAction.js
@@ -38,38 +38,38 @@ export function showResetPasswordNotification (dispatch) {
 }
 
 // for react components
-export function removeLogoutNotification (data) {
-  return dispatch => {
-    dispatch({ type: 'RM_LOGOUT_NOTIFICATION' })
-  }
-}
-
-export function removeLoginNotification (data) {
-  return dispatch => {
-    dispatch({ type: 'RM_LOGIN_NOTIFICATION' })
-  }
-}
-
-export function removeSignupNotification (data) {
-  return dispatch => {
-    dispatch({ type: 'RM_SIGNUP_NOTIFICATION' })
-  }
-}
-
-export function removeUnhandledErrNotification () {
-  return dispatch => {
-    dispatch({ type: 'RM_UNHANDLED_ERR_NOTIFICATION' })
-  }
-}
-
-export function removeInvalidTokenNotification () {
-  return dispatch => {
-    dispatch({ type: 'RM_INVALID_TOKEN_NOTIFICATION' })
-  }
-}
-
-export function removeResetPasswordNotification () {
-  return dispatch => {
-    dispatch({ type: 'RM_RESET_PASSWORD_NOTIFICATION' })
-  }
-}
+// export function removeLogoutNotification (data) {
+//   return dispatch => {
+//     dispatch({ type: 'RM_LOGOUT_NOTIFICATION' })
+//   }
+// }
+//
+// export function removeLoginNotification (data) {
+//   return dispatch => {
+//     dispatch({ type: 'RM_LOGIN_NOTIFICATION' })
+//   }
+// }
+//
+// export function removeSignupNotification (data) {
+//   return dispatch => {
+//     dispatch({ type: 'RM_SIGNUP_NOTIFICATION' })
+//   }
+// }
+//
+// export function removeUnhandledErrNotification () {
+//   return dispatch => {
+//     dispatch({ type: 'RM_UNHANDLED_ERR_NOTIFICATION' })
+//   }
+// }
+//
+// export function removeInvalidTokenNotification () {
+//   return dispatch => {
+//     dispatch({ type: 'RM_INVALID_TOKEN_NOTIFICATION' })
+//   }
+// }
+//
+// export function removeResetPasswordNotification () {
+//   return dispatch => {
+//     dispatch({ type: 'RM_RESET_PASSWORD_NOTIFICATION' })
+//   }
+// }
