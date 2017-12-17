edd4aae57d9187215422fe318b425c717d46f0e0
BeijiYang <langyuyang@outlook.com>
2017-12-17 21:48:01 +0800

    load ProfileSettings asynchronously

--- client/src/containers/ProfileSettingsContainer.js
+++ client/src/containers/ProfileSettingsContainer.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
-import ProfileSettings from '../components/Profile/ProfileSettings'
+import Loadable from 'react-loadable'
+import LoadingComponent from '../components/common/Loading'
+
+const AsyncProfileSettings = Loadable({
+  loader: () => import('../components/Profile/ProfileSettings'),
+  loading: LoadingComponent,
+  delay: 300
+})
 
 class ProfileSettingsContainer extends Component {
 
   render () {
     return (
-      <ProfileSettings />
+      <AsyncProfileSettings />
     )
   }
 }
