ee69c8018f88f649eaee69277c0b780608a06088
BeijiYang <langyuyang@outlook.com>
2017-12-26 17:33:15 +0800

    refactor PayContainer

--- client/src/containers/PayContainer.js
+++ client/src/containers/PayContainer.js
@@ -1,19 +1,13 @@
 
-import React, { Component } from 'react'
+import React from 'react'
 import TopHeader from './TopHeaderContainer'
 import Footer from '../components/Footer/Footer'
 import Pay from '../components/Pay/Pay'
 
-class PayContainer extends Component {
-  render () {
-    return (
-      <div>
-        <TopHeader />
-        <Pay {...this.props} />
-        <Footer />
-      </div>
-    )
-  }
-}
-
-export default PayContainer
+export default props => (
+  <div>
+    <TopHeader />
+    <Pay {...props} />
+    <Footer />
+  </div>
+)
