71cbc61aa4bc5c33318461d8c0c102a2558d9496
BeijiYang <langyuyang@outlook.com>
2018-01-06 21:09:38 +0800

    hqcat sms config

--- api/api/msg.js
+++ api/api/msg.js
@@ -28,7 +28,7 @@ exports.send = (req, res) => {
       PhoneNumbers: phoneNum,
       SignName: config.SignName,
       TemplateCode: config.TemplateCode,
-      TemplateParam: `{"number": "${JSON.stringify(smsCode)}"}`,
+      TemplateParam: `{"code": "${JSON.stringify(smsCode)}"}`,
     })
     .then(
       function(smsRes) {
