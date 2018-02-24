550f6d64313bf811eae110a3d065aeea59441e34
Billie Zhang <billiecoder@gmail.com>
2018-02-24 22:29:50 +0800

    add favicon and change global style

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './containers/App'
 import store from './redux/store'
 import { Provider } from 'react-redux'
-import './assets/global.css'
+import './assets/css/global.css'
 
 ReactDOM.render(
   <Provider store={store}>
