9fc6a95a77164130b07b604121ef33d1bfd92e8b
Peter Wang <happypeter1983@gmail.com>
2018-06-28 17:44:55 +0800

    show course index page #721

--- api/api/courses.js
+++ api/api/courses.js
@@ -2,7 +2,8 @@ const Course = require('../models/course')
 
 exports.single = (req, res) => {
   const { courseName } = req.params
-  Course.findOne({ courseName })
+  const uid = courseName
+  Course.findOne({ uid })
     .then(course => {
       if (!course) return
       res.status(200).json({ success: true, course })
