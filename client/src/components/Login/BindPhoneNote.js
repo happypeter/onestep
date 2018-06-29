a8b9a0d7b332fb761826b0674c610582f5930510
Peter Wang <happypeter1983@gmail.com>
2018-06-29 13:32:40 +0800

    JSS

--- client/src/components/Login/BindPhoneNote.js
+++ client/src/components/Login/BindPhoneNote.js
@@ -1,3 +1,20 @@
 import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import { Link } from 'react-router-dom'
 
-export default () => <div>网站升级，请先到注册页面上绑定手机并设置新的密码</div>
+const styles = theme => ({
+  root: {
+    textAlign: 'center',
+    padding: 20
+  }
+})
+
+const BindPhoneNote = ({ classes: s }) => (
+  <div className={s.root}>
+    网站升级，请先到
+    <Link to="/signup">注册页面</Link>
+    上绑定手机并设置新的密码
+  </div>
+)
+
+export default withStyles(styles)(BindPhoneNote)
