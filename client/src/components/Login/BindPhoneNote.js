f2a5c47efadda57b6f0f2569897bd734f476a5df
Peter Wang <happypeter1983@gmail.com>
2018-07-05 20:22:00 +0800

    noti

--- client/src/components/Login/BindPhoneNote.js
+++ client/src/components/Login/BindPhoneNote.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import { Link } from 'react-router-dom'
-import PersonIcon from '@material-ui/icons/Person'
+import NotificationIcon from '@material-ui/icons/NotificationsActive'
 import { Typography } from '@material-ui/core'
 
 const styles = theme => ({
@@ -18,11 +18,11 @@ const styles = theme => ({
 
 const BindPhoneNote = ({ classes: s }) => (
   <div className={s.root}>
-    <PersonIcon className={s.icon} />
+    <NotificationIcon className={s.icon} />
     <Typography>
-      由于网站升级，所有用户都需要先到
+      Hey..网站升级啦，所以老用户也需要先到
       <Link to="/signup">注册页面</Link>
-      上绑定手机并设置新的密码。老用户注意一定要用自己原来的用户名注册，这样以前购买过的课程会依旧可以观看
+      上绑定手机并设置新的密码，注意一定要用自己原来的用户名注册，这样以前购买过的课程才会依旧保有观看权限哦。
     </Typography>
   </div>
 )
