a3daaeeb596c8181ec7c82eb06a46de094bb826a
Peter Wang <happypeter1983@gmail.com>
2018-07-05 13:48:35 +0800

    price tag

--- client/src/components/Course/Buy.js
+++ client/src/components/Course/Buy.js
@@ -1,20 +1,34 @@
 import React from 'react'
-
+import Button from '@material-ui/core/Button'
 import { withStyles } from '@material-ui/core/styles'
 
 const styles = theme => ({
-  root: {}
+  root: {
+    textAlign: 'center'
+  },
+  priceTag: {
+    fontSize: `2em`,
+    padding: theme.spacing.unit * 2
+  }
 })
 
 class Buy extends React.Component {
   render() {
     const { classes: s, price } = this.props
-    return (
-      <div>
-        <div className={s.root}>Buy</div>
+    const isFree = price === '0'
 
-        <div>{price}</div>
+    const buyButton = (
+      <div>
+        <div className={s.priceTag}>{price}元</div>
+        <Button variant="raised" color="primary">
+          购买
+        </Button>
       </div>
+    )
+
+    const content = isFree ? '免费课程' : buyButton
+    return (
+      <div className={s.root}>{content}</div>
 
       // {price === 0 ? (
       //   <BuyCourseButton />
