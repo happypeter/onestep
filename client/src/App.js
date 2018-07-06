ff39763ee72fbf043614774b0dbc6b00d02e3e16
Peter Wang <happypeter1983@gmail.com>
2018-07-06 12:02:10 +0800

    fix history

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { PureComponent } from 'react'
 // eslint-disable-next-line
+import { Router } from 'react-static'
 import { hot } from 'react-hot-loader'
 import { Provider } from 'react-redux'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import 'typeface-roboto'
+import history from './utils/routerUtils'
 import store from './store'
 import Main from './components/Main'
 
@@ -18,13 +20,17 @@ class App extends PureComponent {
     }
   }
 
+  // NOTE: history 如果在 Main.js 中导入就会失灵，必须要在这里导入
+
   render () {
     return (
       <Provider store={store}>
-        <div>
-          <CssBaseline />
-          <Main />
-        </div>
+        <Router history={history}>
+          <div>
+            <CssBaseline />
+            <Main />
+          </div>
+        </Router>
       </Provider>
     )
   }
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react'
 import Routes from 'react-static-routes'
-import { Router } from 'react-static'
-import { history } from '../utils/routerUtils'
 import Layout from '../containers/LayoutContainer'
 
 class Main extends Component {
   render () {
     return (
-      <Router history={history}>
-        <Layout>
-          <Routes />
-        </Layout>
-      </Router>
+      <Layout>
+        <Routes />
+      </Layout>
     )
   }
 }
