143569c9b887f7c6f53b4353179cd6d7871c785f
Billie Zhang <billiecoder@gmail.com>
2018-11-04 09:47:14 +0800

    only use react-hot-loader in dev env

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { PureComponent } from 'react'
 // eslint-disable-next-line
 import { Router } from 'react-static'
-import { hot } from 'react-hot-loader'
 import { Provider } from 'react-redux'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import 'typeface-roboto'
@@ -36,4 +35,9 @@ class App extends PureComponent {
   }
 }
 
-export default hot(module)(App)
+if (process.env.REACT_STATIC_ENV !== 'production') {
+  const { hot } = require('react-hot-loader')
+  module.exports = hot(module)(App)
+} else {
+  module.exports = App
+}
